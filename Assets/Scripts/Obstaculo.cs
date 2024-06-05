d85bb65deb779393c656c4080844d76f98cee0dc
gabycamilobr <gabycamilobr@gmail.com>
2024-06-05 13:21:45 -0300

    Flappy Bird
    ajustes

--- Assets/Scripts/Obstaculo.cs
+++ Assets/Scripts/Obstaculo.cs
@@ -6,15 +6,27 @@ public class Obstaculo : MonoBehaviour
 {
     [SerializeField]
     private float velocidade = 0.6f;
+    [SerializeField]
+    private float variacaoY;
     // Start is called before the first frame update
     void Start()
     {
-        
+        this.transform.Translate(Vector3.up * Random.Range(-variacaoY, variacaoY));
     }
 
     // Update is called once per frame
     void Update()
     {
         this.transform.Translate(Vector3.left * velocidade * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        this.Destruir();
+    }
+
+    void Destruir()
+    {
+        GameObject.Destroy(this.gameObject);
     }
 }
